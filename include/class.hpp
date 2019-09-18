cd538ad29b855c95f3cab378908df10cf7b6684a
Peter-Jan Gootzen <peterjan@gootzen.net>
2019-09-18 23:51:18 +0200

    Improved CMake project structure

---
+++ include/class.hpp
@@ -0,0 +1,4 @@
+// This is just to demonstrate that the header files work in the beast arena game
+class Ding {
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include "brickengine/engine.hpp"
+#include "engine.hpp"
+#include "class.hpp"
 
 int main(int argc, char** argv) {
     auto engine = BrickEngine();
